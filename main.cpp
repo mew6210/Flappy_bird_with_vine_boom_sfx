9db047a93a8b685d0d0d7e77624e15eef5c82e3b
mew6210 <mew.62@wp.pl>
2025-02-20 15:52:28 +0100

    cleaning up, downloading raylib
    -changed the names of the files
    - included raylib in cmake

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include "stuff.h"
+#include "raylib.h"
+
+int main()
+{
+
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Totally normal flappybird");
+
+
+    while (!WindowShouldClose())
+    {
+       
+        
+        BeginDrawing();
+
+        ClearBackground(Color(0,0,0));
+
+       
+        EndDrawing();
+    }
+
+
+}

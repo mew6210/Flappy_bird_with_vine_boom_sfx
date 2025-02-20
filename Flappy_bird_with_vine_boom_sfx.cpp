278c63817a21467ec07b83114aa848591eb51899
mew6210 <mew.62@wp.pl>
2025-02-20 15:37:45 +0100

    Add project files.

---
+++ Flappy_bird_with_vine_boom_sfx.cpp
@@ -0,0 +1,12 @@
+﻿// Flappy_bird_with_vine_boom_sfx.cpp : Defines the entry point for the application.
+//
+
+#include "Flappy_bird_with_vine_boom_sfx.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
